60a4e0549e3dca25ddec5a58e76010b3beba639a
Giovanni Molina <giovanni.m.05@gmail.com>
2024-09-24 01:53:25 +0100

    desafio guiado

---
+++ desafio/script.js
@@ -0,0 +1,31 @@
+const traerDatos = (
+    () => {
+        const contenedor = document.querySelector('#user-data');
+        // const contenedor = document.getElementById('user-data');
+
+        const mostrarDatos = async() => {
+            let respuesta = await fetch("https://randomuser.me/api/?results=10")
+            let data = await respuesta.json();
+            console.log(data);
+
+            data.results.forEach(user => {
+                contenedor.innerHTML += `
+                    <div>
+                        <img src="${user.picture.large}"/>
+                        <p> ${user.name.first} ${user.name.last} </p>
+                        <p> ${user.email}</p>
+                        <p> ${user.cell}</p>
+                    </div>
+                `
+            });
+        }
+
+
+        return {
+            mostrarDatos
+        }
+
+    }
+ )();
+
+ traerDatos.mostrarDatos();
